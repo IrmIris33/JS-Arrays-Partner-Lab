7b5b51f3d9d65ad141bbb4f4b72297e7ead105ee
Kalyn Smith <kalyn@Kalyns-MacBook-Air.local>
2020-11-23 16:44:16 -0500

    part two

--- Array.js
+++ Array.js
@@ -1,10 +1,15 @@
-let myarray = [5];
+// let myarray = [5];
 
-if (myarray.length <= 0) { 
-    console.log("This is empty")
-} else {
-    console.log("This is correct")
-}
+// if (myarray.length <= 0) { 
+//     console.log("This is empty")
+// } else {
+//     console.log("This is correct")
+// }
 
-let message = (myarray.length === 0) ? "This is empty" : "This is correct";
-console.log(message);
+// let message = (myarray.length === 0) ? "This is empty" : "This is correct";
+// console.log(message);
+
+let myArray = [ 100, 6, 5]
+let newString = "Kalyn"
+myArray.fill(newString)
+console.log(myArray)
