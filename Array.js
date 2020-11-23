2a172539cb979f697c755b70af7d4300ebaaeadc
michael-mcmasters <jmcmasters411@gmail.com>
2020-11-23 17:06:49 -0500

    Finish step 3

--- Array.js
+++ Array.js
@@ -1,6 +1,6 @@
 // let myarray = [5];
 
-// if (myarray.length <= 0) { 
+// if (myarray.length <= 0) {
 //     console.log("This is empty")
 // } else {
 //     console.log("This is correct")
@@ -9,7 +9,18 @@
 // let message = (myarray.length === 0) ? "This is empty" : "This is correct";
 // console.log(message);
 
-let myArray = [ 100, 6, 5]
-let newString = "Kalyn"
-myArray.fill(newString)
-console.log(myArray)
+let myArray = [100] * 10;
+let newString = "Kalyn";
+myArray.fill(newString);
+console.log(myArray);
+
+// const myArr = ["Courtney", "Kaleb", "Froilan", "Tariq"];
+
+// let result = "";
+// for (let i = 0; i < myArr.length; i++) {
+//   result += myArr[i];
+//   if (i < myArr.length - 1) {
+//     result += ", ";
+//   }
+// }
+// console.log(result);
